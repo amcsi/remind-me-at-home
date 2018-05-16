90dddce367ab72b4a3ff058b0ae21a092cb00054
Attila Szeremi <attila@szeremi.org>
2018-05-16 16:39:47 +0100

    Separate NewReminderScreen into its own component

--- App.js
+++ App.js
@@ -1,53 +1,14 @@
 import React from 'react';
-import { KeyboardAvoidingView, SafeAreaView, StyleSheet, TextInput } from 'react-native';
-import { Constants } from 'expo';
-import { Button } from 'react-native-elements';
-import { primaryButtonBackgroundColor } from './data/colors';
+import { SafeAreaView } from 'react-native';
+import NewReminderScreen from './screens/NewReminderScreen';
 
 //noinspection JSUnusedGlobalSymbols
 export default class App extends React.Component {
   render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
-        <KeyboardAvoidingView style={styles.container} behavior="padding">
-          <TextInput
-            multiline
-            style={styles.textInput}
-            placeholder="Write here what you would like to be reminded about when you get home."
-          />
-          <Button
-            large
-            backgroundColor={primaryButtonBackgroundColor}
-            textStyle={styles.primaryButtonText}
-            title="Remind Me At Home!"
-          />
-        </KeyboardAvoidingView>
+        <NewReminderScreen />
       </SafeAreaView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-  },
-  textInput: {
-    width: '100%',
-    flex: 1,
-    paddingLeft: 10,
-    paddingRight: 10,
-    textAlignVertical: 'top',
-    fontSize: 30,
-    borderBottomColor: 'transparent',
-  },
-  primaryButtonButton: {
-    width: '95%',
-  },
-  primaryButtonText: {
-    textAlign: 'center',
-  },
-});
---
+++ screens/NewReminderScreen.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { KeyboardAvoidingView, StyleSheet, TextInput } from 'react-native';
+import { Constants } from 'expo';
+import { Button } from 'react-native-elements';
+import { primaryButtonBackgroundColor } from '../data/colors';
+
+//noinspection JSUnusedGlobalSymbols
+export default class App extends React.Component {
+  render() {
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <TextInput
+          multiline
+          style={styles.textInput}
+          placeholder="Write here what you would like to be reminded about when you get home."
+        />
+        <Button
+          large
+          backgroundColor={primaryButtonBackgroundColor}
+          textStyle={styles.primaryButtonText}
+          title="Remind Me At Home!"
+        />
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+  },
+  textInput: {
+    width: '100%',
+    flex: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    textAlignVertical: 'top',
+    fontSize: 30,
+    borderBottomColor: 'transparent',
+  },
+  primaryButtonButton: {
+    width: '95%',
+  },
+  primaryButtonText: {
+    textAlign: 'center',
+  },
+});
