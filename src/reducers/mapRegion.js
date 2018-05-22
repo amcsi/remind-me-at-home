f2f02cd70b220941bf3ac34d0917441d963c99fa
Attila Szeremi <attila@szeremi.org>
2018-05-22 17:11:41 +0200

    Change default map zoom to be able to point to a home easily

--- src/reducers/mapRegion.js
+++ src/reducers/mapRegion.js
@@ -4,8 +4,8 @@ import { isEqual, pick } from 'lodash';
 const INITIAL_STATE = {
   latitude: 51.5074,
   longitude: 0.1278,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421,
+  latitudeDelta: 0.006,
+  longitudeDelta: 0.0025,
 };
 
 export default function (state = INITIAL_STATE, action) {
